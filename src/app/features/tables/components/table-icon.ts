110d41b8f604c0a14b25824bfdcfccb1759d767e
w.alexander <will_alexander@me.com>
2025-08-26 23:36:05 +0200

    Improve table icon

--- src/app/features/tables/components/table-icon.ts
+++ src/app/features/tables/components/table-icon.ts
@@ -5,10 +5,21 @@ import { Component } from '@angular/core';
   template: `
 		<div
 				class="w-12 h-12 mx-auto mb-3 rounded-full bg-white/10 backdrop-blur-sm flex items-center justify-center">
-			<svg class="w-6 h-6" viewBox="0 0 24 24">
-				<path
-						fill="currentColor"
-						d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
+			<svg class="w-6 h-6" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+					 viewBox="0 0 26 26">
+				<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+				<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+				<g id="SVGRepo_iconCarrier">
+					<g>
+						<path fill="currentColor"
+									d="M25.484,7.114l-4.278-3.917C21.034,3.069,20.825,3,20.61,3H5.38C5.165,3,4.956,3.069,4.783,3.197 l-4.38,4C0.403,7.197,0,7.453,0,8v2c0,0.551,0.449,1,1,1h24c0.551,0,1-0.449,1-1V8C26,7.469,25.484,7.114,25.484,7.114z"></path>
+						<path fill="currentColor" d="M2,23c-0.551,0-1-0.449-1-1V10h3v12c0,0.551-0.449,1-1,1H2z"></path>
+						<path fill="currentColor" d="M23,23c-0.551,0-1-0.449-1-1V10h3v12c0,0.551-0.449,1-1,1H23z"></path>
+						<path fill="currentColor" d="M20,18c-0.551,0-1-0.449-1-1v-5h2v5C21,17.551,20.551,18,20,18L20,18z"></path>
+						<path fill="currentColor"
+									d="M6,18c-0.551,0-1-0.449-1-1v-5h2v5C7,17.551,6.551,18,6,18L6,18z"></path>
+					</g>
+				</g>
 			</svg>
 		</div>
   `
