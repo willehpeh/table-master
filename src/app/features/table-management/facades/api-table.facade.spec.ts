92e7254e597ec9fccd68bf644f4732703728d1c7
w.alexander <will_alexander@me.com>
2025-09-10 12:31:25 +0200

    Failing load all tables test

--- src/app/features/table-management/facades/api-table.facade.spec.ts
+++ src/app/features/table-management/facades/api-table.facade.spec.ts
@@ -1,19 +1,40 @@
 import { ApiTableFacade } from './api-table.facade';
 import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { TableService } from '../services/table.service';
+import { ApiTableService } from '../services/api-table.service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ApiTableFacade', () => {
   let facade: ApiTableFacade;
+  let httpCtrl: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApiTableFacade]
+      providers: [
+        ApiTableFacade,
+        provideZonelessChangeDetection(),
+        { provide: TableService, useClass: ApiTableService },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     });
 
     facade = TestBed.inject(ApiTableFacade);
+    httpCtrl = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(facade).toBeTruthy();
+  });
+
+  it('should load all tables if they are not already loaded', () => {
+    const tables = facade.allTables();
+  });
+
+  afterEach(() => {
+    httpCtrl.verify();
   });
 
 });
